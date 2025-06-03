0363f26eb0390f3863ebce689ff3faf4a4360115
Everaldo451 <everaldo.work.jr@gmail.com>
2025-06-03 11:54:10 -0300

    initial file

---
+++ index.js
@@ -0,0 +1,54 @@
+class ImagemZoom {
+
+    constructor(src, zoom) {
+        this.src = src
+        this.zoom = zoom
+    }
+
+    createImage() {
+        const image = document.createElement("img")
+        image.style.width = "100%"
+        image.style.height = "100%"
+        image.style.objectFit = "cover"
+        image.style.objectPosition = "center"
+        return image
+    }
+
+    onImageMouseEnter(e) {
+        const image = e.currentTarget
+        const imagemWidth = image.clientWidth
+        const imagemHeight = image.clientHeight
+        const widthPercent = (e.offsetX/imagemWidth)*100
+        const heightPercent = (e.offsetY/imagemHeight)*100
+        image.style.transform = `translate(${(50-widthPercent)}%, ${(50-heightPercent)}%) scale(2)`
+    }
+
+    onImageMouseLeave(e) {
+        const image = e.currentTarget
+        image.style.transform = "none"
+    }
+
+    render(parentElement) {
+        const imagemContainer = document.createElement("div")
+        imagemContainer.classList.add("container")
+        const imagem = document.createElement("img")
+
+        imagem.addEventListener("mousemove", (e) => {
+            const image = e.currentTarget
+            const imagemWidth = image.clientWidth
+            const imagemHeight = image.clientHeight
+            const widthPercent = (e.offsetX/imagemWidth)*100
+            const heightPercent = (e.offsetY/imagemHeight)*100
+            image.style.transform = `translate(${(50-widthPercent)}%, ${(50-heightPercent)}%) scale(2)`
+        })
+
+        imagem.addEventListener("mouseleave", (e) => {
+            const image = e.currentTarget
+            image.style.transform = "none"
+        })
+
+        imagem.src = this.imagemCamisa
+        imagemContainer.appendChild(imagem)
+        parentElement.appendChild(imagemContainer)
+    }
+}
